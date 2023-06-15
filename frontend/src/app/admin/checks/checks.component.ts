ea129f97b17ed1c22e51c13b0aec5672f8b2e95a
semon saleeb <semonsaleeb@gmail.com>
2023-06-15 11:28:27 +0200

    check v4

--- frontend/src/app/admin/checks/checks.component.ts
+++ frontend/src/app/admin/checks/checks.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ChecksOrder } from 'src/app/interfaces/checks-order';
-import { Order } from 'src/app/interfaces/order';
 import { Users } from 'src/app/interfaces/users';
 import { OrderService } from 'src/app/services/order.service';
 import { UsersService } from 'src/app/services/users.service';
@@ -14,52 +13,39 @@ import { UsersService } from 'src/app/services/users.service';
 export class ChecksComponent {
   users !: Users[];
   checks !: ChecksOrder[];
-  orders!: Order[];
 
   constructor(private usersService: UsersService, private orderService: OrderService) { }
 
   ngOnInit() {
     this.usersService.getUsers().subscribe((res: any) => {
       this.users = res;
+      // console.log(this.users);
     });
 
+    // this.orderService.getOrders().subscribe((res: any) => {
+    //   this.checks = res;
+    //   console.log(this.checks);
+
+    // })
   }
   start_date: any = '';
   end_Date: any = '';
   user: any = '';
-  userName: any = '';
+  data !: any[];
+  onSubmit(startDate: NgForm, end_Date: NgForm, user: NgForm) {
+    this.start_date = startDate;
+    console.log(this.start_date);
+    console.log(end_Date);
+    // console.log(user);
+    let selectedMembers = this.checks.filter(m => {
+      if ( m.created_at > this.start_date &&  m.created_at< this.end_Date ) 
+        console.log(m);
+          
+        // this.checks=m
 
-  onSubmit() {
-    if (Number(this.user) >= 1) {
-      this.users.filter(u => { if (u.id == Number(this.user)) this.userName = u.Name })
-      this.orderService.getOrderByUserId(Number(this.user)).subscribe((res: any) =>{
-        this.checks = res
-        if (this.start_date && this.end_Date) {
-          const startDate = new Date(this.start_date);
-          const endDate = new Date(this.end_Date);
-          this.checks = this.checks.filter((order: any) => {
-            const orderDate = new Date(order.created_at);
-            return orderDate >= startDate && orderDate <= endDate;
-          });
-        }
-      }
-      )
-    } else {
-      this.orderService.getOrders().subscribe((res: any) => {
-        this.checks = res
-        if (this.start_date && this.end_Date) {
-          const startDate = new Date(this.start_date);
-          const endDate = new Date(this.end_Date);
-          this.checks = this.checks.filter((order: any) => {
-            const orderDate = new Date(order.created_at);
-            return orderDate >= startDate && orderDate <= endDate;
-          });
-        }
-      });
+});
 
-    }
-
-
+// console.log(selectedMembers);
 
   }
 }
